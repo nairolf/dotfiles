d6cfc0771de0107e41adf983938b3c86d47de55c
Florian Hanno <florian.hanno@twt.de>
2019-06-11 12:23:36 +0200

    changed config order to allow zsh tmux plugin to work

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -8,16 +8,6 @@ export TERM=xterm-256color
 ANTIGEN_PATH=~/dotfiles/antigen
 ZSH_PATH=~/dotfiles/zsh
 
-source $ANTIGEN_PATH/bin/antigen.zsh
-
-antigen init $ZSH_PATH/antigenrc.sh
-
-source $ZSH_PATH/aliases.sh
-
-## theme options
-# powerline10k
-POWERLEVEL9K_MODE='nerdfont-complete'
-
 ## plugin options
 
 # zsh-autosuggestions (https://github.com/zsh-users/zsh-autosuggestions)
@@ -26,6 +16,16 @@ ZSH_AUTOSUGGEST_USE_ASYNC=true
 
 # tmux (https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#tmux)
 # Automatically start a tmux session upon logging in
-ZSH_TMUX_AUTOSTART=false
+ZSH_TMUX_AUTOSTART='true'
 #When running tmux, the variable $TERM is supposed to be set to screen or one of its derivatives. This option will set the default-terminal option of tmux to screen-256color if 256 color terminal support is detected, and screen otherwise.
 ZSH_TMUX_FIXTERM=true
+
+## theme options
+# powerline10k
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+source $ANTIGEN_PATH/bin/antigen.zsh
+
+antigen init $ZSH_PATH/antigenrc.sh
+
+source $ZSH_PATH/aliases.sh
