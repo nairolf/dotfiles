d81ed80b6f890f2da4ca297ee346794f2a5d22e7
Florian Hanno <florian.hanno@twt.de>
2019-06-19 10:52:41 +0200

    added continuum-restore

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -24,7 +24,7 @@ ZSH_AUTOSUGGEST_USE_ASYNC=true
 
 # tmux (https://github.com/robbyrussell/oh-my-zsh/wiki/Plugins#tmux)
 # Automatically start a tmux session upon logging in
-ZSH_TMUX_AUTOSTART='true'
+ZSH_TMUX_AUTOSTART='false'
 #When running tmux, the variable $TERM is supposed to be set to screen or one of its derivatives. This option will set the default-terminal option of tmux to screen-256color if 256 color terminal support is detected, and screen otherwise.
 ZSH_TMUX_FIXTERM=true
 
@@ -34,7 +34,7 @@ POWERLEVEL9K_MODE='nerdfont-complete'
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time dir_writable ssh php_version )
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time php_version ip)
 
 source $ANTIGEN_PATH/bin/antigen.zsh
 
